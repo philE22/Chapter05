ae43acb4cd90c9f12c60096daf4b779c858065df
philE22 <moonwhlking@gmail.com>
2022-06-08 18:46:46 +0900

    ~p.322 Chapter05 양방향맵핑 끝

--- Chapter05/src/main/java/com/rubypaper/domain/Board.java
+++ Chapter05/src/main/java/com/rubypaper/domain/Board.java
@@ -9,16 +9,25 @@ import java.util.Date;
 
 @Getter
 @Setter
-@ToString
+@ToString(exclude = "member")
 @Entity
 public class Board {
     @Id
     @GeneratedValue
     private Long seq;
     private String title;
-    private String writer;
+//    private String writer;
     private String content;
     @Temporal(value = TemporalType.TIMESTAMP)
     private Date createDate;
     private Long cnt;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID", nullable = false)
+    private Member member;
+
+    public void setMember(Member member) {
+        this.member = member;
+        member.getBoardList().add(this);
+    }
 }
--- Chapter05/src/main/java/com/rubypaper/persistence/BoardRepository.java
+++ Chapter05/src/main/java/com/rubypaper/persistence/BoardRepository.java
@@ -20,17 +20,17 @@ public interface BoardRepository extends CrudRepository<Board, Long> {
 
     List<Board> findByTitleContainingOrderBySeqDesc(String searchKeyword);
 
-    @Query("SELECT b FROM Board b " +
-            "WHERE b.title like %:searchKeyword% " +
-            "ORDER BY b.seq DESC")
-    List<Board> queryAnnotationTest1(@Param("searchKeyword") String searchKeyword);
-
-    @Query("SELECT b.seq, b.title, b.writer, b.createDate FROM Board b WHERE b.title like %?1% ORDER BY b.seq DESC")
-    List<Object[]> queryAnnotationTest2(@Param("searchKeyword") String searchKeyword);
-
-    @Query(value = "select seq, title, writer, create_date, from board where title like '%'||?1||'%' order by seq desc", nativeQuery=true )
-    List<Object[]> queryAnnotationTest3(String searchKeyword);
-
-    @Query("SELECT b FROM Board b ORDER BY b.seq DESC")
-    List<Board> queryAnnotationTest4(Pageable paging);
+//    @Query("SELECT b FROM Board b " +
+//            "WHERE b.title like %:searchKeyword% " +
+//            "ORDER BY b.seq DESC")
+//    List<Board> queryAnnotationTest1(@Param("searchKeyword") String searchKeyword);
+//
+//    @Query("SELECT b.seq, b.title, b.writer, b.createDate FROM Board b WHERE b.title like %?1% ORDER BY b.seq DESC")
+//    List<Object[]> queryAnnotationTest2(@Param("searchKeyword") String searchKeyword);
+//
+//    @Query(value = "select seq, title, writer, create_date, from board where title like '%'||?1||'%' order by seq desc", nativeQuery=true )
+//    List<Object[]> queryAnnotationTest3(String searchKeyword);
+//
+//    @Query("SELECT b FROM Board b ORDER BY b.seq DESC")
+//    List<Board> queryAnnotationTest4(Pageable paging);
 }
